841d76d9b23cb9382a7920ed3029cf51213013c2
Jonathan Eatherly <jonathan.eatherly@involver.com>
2011-10-15 20:50:54 -0700

    Janky now loading templates, and rendering initial feed.

--- src/javascripts/init.js
+++ src/javascripts/init.js
@@ -4,8 +4,15 @@ var JANKY_INCLUDES = {
   ],
   javascript: [
     chrome.extension.getURL("javascripts/jquery-1.6.4.min.js"),
+    chrome.extension.getURL("javascripts/jquery.jfeed.pack.js"),
     chrome.extension.getURL("javascripts/underscore-min.js"),
     chrome.extension.getURL("javascripts/janky.js")
+  ],
+  templates: [
+    {
+      url: chrome.extension.getURL("views/job.template.html"),
+      title: 'job'
+    }
   ]
 };
 
@@ -19,7 +26,7 @@ $(document).ready(function(){
 
     var init = function(){
       // Ghetto check for if on jenkins page...Should refactor
-      if ($('#footer a').attr('href') !== "http://jenkins-ci.org/"){ return; }
+      if ($('#footer > a').attr('href') != "http://jenkins-ci.org/"){ return; }
       jenkins = true;
 
       run_includes();
@@ -39,10 +46,18 @@ $(document).ready(function(){
     };
 
     var inject_index = function(){
-      $('body').append('<div id="janky_wrap"></div>');
-      $('#janky_wrap').load(JANKY_INDEX, function(body){
-        console.log('remote index loaded');
+      $('body').append('<div id="janky_fixed"></div>');
+      $('#janky_fixed').load(JANKY_INDEX, function(body){
+        inject_templates();
       });
+    };
+
+    var inject_templates = function(){
+      $('#janky_fixed').append('<div id="janky_templates" style="display:none;"></div>');
+      for (var i = 0; i < JANKY_INCLUDES.templates.length; i++){
+        $('#janky_templates').append('<div data-template-name="' + JANKY_INCLUDES.templates[i].title + '"></div>');
+        $('#janky_templates > div:last').load(JANKY_INCLUDES.templates[i].url);
+      }
     };
 
     var is_jenkins = function(){
--- src/javascripts/janky.js
+++ src/javascripts/janky.js
@@ -1,59 +1,109 @@
 (function(){
   var self = { };
+  var m_ = { };
   
   // Private Attributes
-  var window_height = 0;
-  var window_width = 0;
-  var janky_drawer_offest = 0;
-  var janky_wrap = null;
-  var janky_drawer_toggle = null;
-  var janky_drawer_shown = false;
+  m_.window_height = 0;
+  m_.window_width = 0;
+  m_.janky_drawer_offest = 0;
+  m_.janky_wrap = null;
+  m_.janky_content = null;
+  m_.janky_drawer_toggle = null;
+  m_.janky_drawer_shown = false;
+  m_.janky_initial_render = false;
+  m_.jenkins_data = { };
 
-  var init = function(){
-    janky_wrap = $('#janky_wrap');
-    janky_drawer_toggle = $('#janky_wrap #janky_drawer_toggle');
-    window_height = $(window).height();
-    window_width = $(window).width();
+  m_.init = function(){
+    m_.janky_fixed = $('#janky_fixed');
+    m_.janky_wrap = $('#janky_wrap');
+    m_.janky_content = $('#janky_content');
+    m_.janky_drawer_toggle = $('#janky_fixed #janky_drawer_toggle');
 
-    init_janky_wrap();
-  };
+    m_.update_janky_sizes();
 
-  // Private Methods
-  function init_janky_wrap(){
-    update_janky_sizes();
-    janky_wrap.css({
-      'bottom':janky_drawer_offest
+    m_.janky_fixed.css({
+      'bottom': -(m_.window_height)
     });
 
     // Attach drawer toggle
-    janky_drawer_toggle.bind('click', toggle_janky_drawer);
+    m_.janky_drawer_toggle.bind('click', m_.toggle_janky_drawer);
 
-    // Attach window size change
-    // $(window).bind('resize', update_janky_position);
+    // $(window).bind('resize', m_.update_janky_sizes);
 
-    janky_wrap.show();
-  }
+    m_.get_jenkins_feed();
+    m_.janky_fixed.show();
+    m_.render_janky_feed();
+  };
 
-  function update_janky_sizes(){
-    janky_drawer_offest = janky_drawer_toggle.height();
-  }
+  // Public Methods
+  self.update_janky_feed = function(){
+    m_.jenkins_data = { };
+    m_.get_jenkins_feed();
+  };
 
-  function update_janky_position(e){
-    if (janky_drawer_shown){ return true; }
-    update_janky_sizes();
+  // Private Methods
 
-    console.log(e);
-  }
+  m_.get_jenkins_feed = function(){
+    jQuery.getFeed({
+      url: '/rssAll',
+      success: function(feed){
+        m_.parse_jenkins_feed(feed.items);
+      }
+    });
+  };
 
-  function toggle_janky_drawer(){
-    update_janky_sizes();
-    var toogle_offest = janky_drawer_shown ? 0: janky_drawer_offest;
-    janky_wrap.animate({
+  m_.parse_jenkins_feed = function(feed){
+    var build_title = '';
+    var build_number = '';
+    var build_state = '';
+    var build_time = null;
+    for (var i = 0; i < feed.length; i++){
+      build_title = feed[i].title.split(' ')[0];
+      if (typeof(m_.jenkins_data[build_title]) == 'undefined'){
+        build_time = feed[i].updated;
+        build_number = feed[i].title.split(' ')[1];
+        build_state = feed[i].title.split(' ')[2].replace('(', '').replace(')', '');
+        m_.jenkins_data[build_title] = {
+          'title': build_title,
+          'number': build_number,
+          'state': build_state,
+          'date': build_time
+        };
+      }
+    }
+    m_.render_janky_feed();
+  };
+
+  m_.render_janky_feed = function(){
+    console.log('render feed called');
+    var job_template = $('#janky_templates div[data-template-name="job"]').html();
+    console.log(job_template);
+
+    for (var key in m_.jenkins_data){
+      m_.janky_content.append(job_template);
+    }
+  };
+
+  m_.update_janky_sizes = function(){
+    m_.window_width = $(window).width();
+    m_.window_height = $(window).height();
+    // m_.janky_drawer_offest = m_.janky_drawer_toggle.height() - 20;
+
+    m_.janky_wrap.css({
+      'height': m_.window_height
+    });
+  };
+
+  m_.toggle_janky_drawer = function(){
+    m_.update_janky_sizes();
+    var toogle_offest = m_.janky_drawer_shown ? -(m_.window_height): -60;
+    m_.janky_fixed.animate({
       bottom: toogle_offest
     }, 300);
-    janky_drawer_shown = !janky_drawer_shown;
-  }
 
-  init();
+    m_.janky_drawer_shown = !m_.janky_drawer_shown;
+  };
+
+  m_.init();
   return self;
 }( ));
